ade23024f39810deeebe383de7f15b088bc4a650
yeanat2025-cmyk <yeanat2025@gmail.com>
2026-03-15 22:38:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-function generateTRX(){
-return Math.random().toString(36).substring(2,10).toUpperCase();
-}
-
-function sendSMS(){
+async function sendSMS(){
 
 let phone=document.getElementById("number").value;
 let amount=document.getElementById("amount").value;
 
-let trx=generateTRX();
+let trx=Math.random().toString(36).substring(2,10).toUpperCase();
 
-let message=`AIDE PAY
+let message="AIDE PAY\nYou received Tk "+amount+"\nTRX "+trx;
 
-You have received Tk ${amount}
-TRX ID: ${trx}`;
+try{
 
-fetch("https://api.textbee.dev/api/v1/gateway/devices/69b625e6eb0b29634caa6c96/send-sms",{
+let res=await fetch("https://api.textbee.dev/api/v1/gateway/devices/69b625e6eb0b29634caa6c96/send-sms",{
 method:"POST",
 headers:{
-"Content-Type":"application/json",
-"x-api-key":"4034d517-eaaa-47b7-9dfc-58f83486e0ee"
+"x-api-key":"4034d517-eaaa-47b7-9dfc-58f83486e0ee",
+"Content-Type":"application/json"
 },
 body:JSON.stringify({
 recipients:[phone],
 message:message
 })
-})
-.then(response=>response.json())
-.then(data=>{
-console.log(data);
+});
+
+let data=await res.json();
 
 if(data && data.data){
+
 document.getElementById("result").innerHTML=
 "✅ Payment Successful<br>TRX ID: "+trx;
+
 }else{
+
 document.getElementById("result").innerHTML=
 "❌ SMS Failed";
+
 }
-})
-.catch(error=>{
+
+}catch(e){
+
 document.getElementById("result").innerHTML=
 "❌ Network Error";
-console.log(error);
-});
+
+}
 
 }
